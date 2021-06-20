b5850927389fc031d49b45903c96faf7f4f34c66
Ágata Velasco Penze <avpenze@gmail.com>
2021-06-20 12:36:44 -0400

    Consumindo Api prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import { GetStaticProps } from 'next';
-
+import Head from 'next/head';
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
-
+import Link from 'next/link';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
@@ -24,13 +25,44 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>spacetraveling | posts</title>
+      </Head>
+      <main>
+        <div>
+        </div>
+      </main>
+    </>
+  )
+ }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps : GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query([
+    Prismic.Predicates.at('document.type', 'posts')
+  ], {
+    fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+    pageSize: 10,
+  });
 
-//   // TODO
-// };
+  console.log(JSON.stringify(postsResponse, null, 2));
+
+  const posts = postsResponse.results.map(posts => {
+    return {
+      slug: posts.uid,
+      title: posts.data.title,
+      subtitle: posts.data.subtitle,
+      author: posts.data.author,
+    }
+  });
+
+  return {
+    props: {
+      posts
+    }
+  }
+
+};
